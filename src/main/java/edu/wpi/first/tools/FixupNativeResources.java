e82a0e1feb96608b3855cd1c35a2774108241637
Thad House <thadhouse1@gmail.com>
2020-06-27 16:30:29 -0700

    Get mac working

--- src/main/java/edu/wpi/first/tools/FixupNativeResources.java
+++ src/main/java/edu/wpi/first/tools/FixupNativeResources.java
@@ -1,6 +1,9 @@
 package edu.wpi.first.tools;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.util.List;
+import java.util.ArrayList;
 
 import javax.inject.Inject;
 
@@ -56,13 +59,64 @@ public class FixupNativeResources extends DefaultTask {
             // Strip all binaries
             Directory directory = outputDirectory.get();
             for (File file : directory.getAsFileTree()) {
+                if (!file.isFile()) {
+                    continue;
+                }
                 project.exec((ex) -> {
                     ex.commandLine("strip", "--strip-all", "--discard-all", file.toString());
                 });
             }
         }
 
-        // TODO Platform specific fixups
-    }
+        if (OperatingSystem.current().isMacOsX()) {
+            // Set rpath correctly in all binaries
+            Directory directory = outputDirectory.get();
 
+            List<File> allFilesList = new ArrayList<>();
+            List<String> fileNameList = new ArrayList<>();
+            for (File file : directory.getAsFileTree()) {
+                if (!file.isFile()) {
+                    continue;
+                }
+                allFilesList.add(file);
+                fileNameList.add(file.getName());
+            }
+
+            List<String> filesToFixup = new ArrayList<>();
+
+            for (File file : allFilesList) {
+
+                // Get list of all dependent binaries
+                ByteArrayOutputStream standardOutput = new ByteArrayOutputStream();
+
+                project.exec((ex) -> {
+                    ex.commandLine("otool", "-L", file.toString());
+                    ex.setStandardOutput(standardOutput);
+                });
+
+                filesToFixup.clear();
+
+                String outputStr = standardOutput.toString();
+                String currentFileName = file.getName();
+
+                // See if any extracted files exist in deps list
+                for (String lookupFile : fileNameList) {
+                    if (lookupFile.equals(currentFileName)) {
+                        continue;
+                    }
+
+                    if (outputStr.contains(lookupFile)) {
+                        filesToFixup.add(lookupFile);
+                    }
+                }
+
+                // Fixup any dependencies
+                for (String fixupFile : filesToFixup) {
+                    project.exec((ex) -> {
+                        ex.commandLine("install_name_tool", "-change", fixupFile, "@loader_path/" + fixupFile, file.toString());
+                    });
+                }
+            }
+        }
+    }
 }
