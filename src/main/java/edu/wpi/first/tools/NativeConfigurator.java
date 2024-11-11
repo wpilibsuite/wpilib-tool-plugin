297feaa933baa435239fb445c558554caad56235
Thad House <ThadHouse@users.noreply.github.com>
2024-11-11 15:52:28 -0800

    Update javafx version (#26)
    mac needs a newer javafx version to fix a bug

--- src/main/java/edu/wpi/first/tools/NativeConfigurator.java
+++ src/main/java/edu/wpi/first/tools/NativeConfigurator.java
@@ -5,7 +5,7 @@ import org.gradle.api.artifacts.dsl.DependencyHandler;
 
 public class NativeConfigurator {
     private final PlatformMapper platformMapper;
-    private String defaultJavaFxVersion = "17.0.2";
+    private String defaultJavaFxVersion = "17.0.13";
     private String wpilibVersion = "+";
     private final DependencyHandler handler;
 
