b07b87193aea3061c01f46f8606a1d0ef05257d0
Thad House <ThadHouse@users.noreply.github.com>
2024-11-11 15:52:36 -0800

    Remove windows loader helper (#27)

--- src/main/java/edu/wpi/first/tools/WpilibTools.java
+++ src/main/java/edu/wpi/first/tools/WpilibTools.java
@@ -7,21 +7,10 @@ import static org.gradle.api.artifacts.type.ArtifactTypeDefinition.ZIP_TYPE;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.gradle.api.UnknownTaskException;
-import org.gradle.internal.os.OperatingSystem;
 
 public class WpilibTools implements Plugin<Project> {
     @Override
     public void apply(Project project) {
-        if (OperatingSystem.current().isWindows()) {
-            var extractTaskName = "extractEmbeddedWindowsHelpers";
-            try {
-                project.getRootProject().getTasks().named(extractTaskName);
-            } catch (UnknownTaskException notfound) {
-                project.getRootProject().getTasks().register(extractTaskName, ExtractEmbeddedWindowsHelpers.class);
-            }
-        }
-
         project.getDependencies().registerTransform(UnzipTransform.class, transform -> {
            transform.getFrom().attribute(ARTIFACT_TYPE_ATTRIBUTE, ZIP_TYPE);
            transform.getTo().attribute(ARTIFACT_TYPE_ATTRIBUTE, DIRECTORY_TYPE);
